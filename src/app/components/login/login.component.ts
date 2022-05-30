5204a0ba096e3a181735ae76ea5eee5c6ef8cd57
Can Thuy <canthuy140499@gmail.com>
2022-05-30 09:36:07 +0700

    add images and login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,42 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$'),
+    ]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // getter formControl
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
   }
 
+  // Login function
+  public login(): void {
+    this.authService.login(this.loginForm.value).subscribe((val) => {
+      console.log(val);
+    });
+  }
+
+  // Demo accc
+  public demoAccount() {
+    this.loginForm.setValue({ email: 'thuy@gmail.com', password: '123456' });
+  }
 }

b49b89ab40cff9a07dba79b29acd562b2a76abae
Can Thuy <canthuy140499@gmail.com>
2022-06-01 00:13:02 +0700

    implement case user is not logout

--- src/app/layouts/common/header/header.component.ts
+++ src/app/layouts/common/header/header.component.ts
@@ -1,15 +1,30 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public show: boolean = false;
+  public isLogin: boolean;
 
-  constructor() { }
+  constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.isLogin = this.authService.isLogin();
   }
 
+  public onToggle() {
+    this.show = !this.show;
+  }
+
+  public onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+    this.show = false;
+    this.isLogin = false;
+  }
 }

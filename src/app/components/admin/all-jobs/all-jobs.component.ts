5442f33119ec231e6b534d221751335ff1b54b29
Hoài An <75045333+ptha299@users.noreply.github.com>
2022-05-31 17:05:22 +0700

    Hoaian (#5)
    * admin-sidebar
    
    * add routing, admin profile, all job displays

--- src/app/components/admin/all-jobs/all-jobs.component.ts
+++ src/app/components/admin/all-jobs/all-jobs.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Job } from 'src/app/models/job.model';
+import { JobService } from 'src/app/services/job.service';
 
 @Component({
   selector: 'app-all-jobs',
   templateUrl: './all-jobs.component.html',
-  styleUrls: ['./all-jobs.component.scss']
+  styleUrls: ['./all-jobs.component.scss'],
 })
 export class AllJobsComponent implements OnInit {
-
-  constructor() { }
+  jobData: Job[] = [];
+  totalJobs: number = 0;
+  constructor(private jobService: JobService) {}
 
   ngOnInit(): void {
+    this.jobService.getAllJobs().subscribe((res: any) => {
+      this.jobData = res.jobs;
+      this.totalJobs = res.totalJobs;
+      console.log(res);
+    });
   }
-
 }

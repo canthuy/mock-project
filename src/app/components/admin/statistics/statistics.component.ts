684b489b31eee9554a2d610eb5839148053ef380
Can Thuy <canthuy140499@gmail.com>
2022-06-01 23:59:35 +0700

    add chart

--- src/app/components/admin/statistics/statistics.component.ts
+++ src/app/components/admin/statistics/statistics.component.ts
@@ -17,6 +17,37 @@ export class StatisticsComponent implements OnInit {
     interview: 0,
     declined: 0,
   };
+  public barChart: boolean = true;
+
+  // Data for chart
+  public series: any[] = [];
+  public data: any[] = [];
+  public view: [number, number] = [700, 300];
+  public legend: boolean = false;
+  public legendPosition = 'below';
+  public legendTitle: string = '';
+  public showLabels: boolean = true;
+  public animations: boolean = true;
+  public xAxis: boolean = true;
+  public yAxis: boolean = true;
+  public timeline: boolean = true;
+  public showXAxisLabel: boolean = true;
+  public showYAxisLabel: boolean = true;
+  public xAxisLabel: string = 'Month';
+  public yAxisLabel: string = 'Count';
+
+  public colorScheme = {
+    domain: [
+      '#683cb7',
+      '#059689',
+      '#ea1e63',
+      '#7aa3e5',
+      '#2a4f74',
+      '#00524b',
+      '#205803',
+    ],
+  };
+
   constructor(
     private jobService: JobService,
     private spinner: NgxSpinnerService
@@ -27,6 +58,28 @@ export class StatisticsComponent implements OnInit {
     this.jobService.getStats().subscribe((res: any) => {
       this.spinner.hide();
       this.totalStats = res.defaultStats;
+      this.series = res.monthlyApplications.map((val) => {
+        return {
+          name: val.date,
+          value: val.count,
+        };
+      });
+      this.data = this.series;
     });
+  }
+
+  // Change chart mode
+  public onChangeChart() {
+    this.barChart = !this.barChart;
+    if (this.barChart) {
+      this.data = this.series;
+    } else {
+      this.data = [
+        {
+          name: 'Applications',
+          series: this.series,
+        },
+      ];
+    }
   }
 }
